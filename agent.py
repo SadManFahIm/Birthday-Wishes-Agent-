b5910d073f6bf60b374af27b20bc1085d843a87d
SadManFahIm <fahimsadman4@gmail.com>
2026-05-21 01:01:18 +0600

    feat: add birthday miss tracker with late wish support

--- agent.py
+++ agent.py
@@ -83,6 +83,7 @@ from occasion_detection import run_occasion_detection
 from multilang_reply import detect_language, get_multilang_reply, build_multilang_instructions
 
 from relationship_health import (init_health_table, run_relationship_health_report)
+from birthday_miss_tracker import (init_miss_table, run_miss_tracker, get_missed_contacts)
 from decay_alert import (init_decay_table, run_decay_alert, get_fading_contacts, build_decay_alert_instructions)
 
 from best_time_connect import (init_activity_table, get_best_send_time,
@@ -2282,6 +2283,8 @@ async def main():
     init_predicted_birthday_table()
 
     init_eq_table()
+
+    init_miss_table()
 
     init_decay_table()
     
